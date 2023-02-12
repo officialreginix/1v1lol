834a6ea62a79d0e51fc685a03ada1fbd1009c8be
officialreginix <62349761+officialreginix@users.noreply.github.com>
2023-02-12 17:34:10 -0500

    Update IronSourceRV.js

--- js/IronSourceRV.js
+++ js/IronSourceRV.js
@@ -1,33 +1,34 @@
-window.InitRV = function InitRV(id) {
-  $.getScript("https://static.ultra-rv.com/rv-min.js", function () {
-
-    let userId = id === undefined ? "Guest" : id;
-    let ironRV = IronRV.getInstance({
-      applicationKey: "1-mb5whk",
-      applicationUserId: userId,
+window.InitRV = function InitRV(id = 'Guest') {
+  $.getScript('https://static.ultra-rv.com/rv-min.js', function() {
+    const ironRV = IronRV.getInstance({
+      applicationKey: '1-mb5whk',
+      applicationUserId: id,
     });
 
-    ironRV.addListener(IronRV.EVENTS.READY, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvReady");
-    });
+    const eventHandlers = {
+      [IronRV.EVENTS.READY]: () => {
+        gameInstance.SendMessage('MainMenuManagers', 'RvReady');
+      },
+      [IronRV.EVENTS.CLOSE]: () => {
+        gameInstance.SendMessage('MainMenuManagers', 'RvWatchComplete', 'false');
+      },
+      [IronRV.EVENTS.COMPLETION]: () => {
+        gameInstance.SendMessage('MainMenuManagers', 'RvWatchComplete', 'true');
+      },
+      [IronRV.EVENTS.AD_BLOCK]: () => {
+        ironRV.showAdBlockMessage();
+      },
+      [IronRV.EVENTS.INIT_ERROR]: () => {
+        // Handle initialization error
+      },
+    };
 
-    ironRV.addListener(IronRV.EVENTS.CLOSE, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "false");
-    });
-
-    ironRV.addListener(IronRV.EVENTS.COMPLETION, function () {
-      gameInstance.SendMessage("MainMenuManagers", "RvWatchComplete", "true");
-    });
-
-    ironRV.addListener(IronRV.EVENTS.AD_BLOCK, function () {
-      ironRV.showAdBlockMessage();
-    });
-
-    ironRV.addListener(IronRV.EVENTS.INIT_ERROR, function () {
+    Object.entries(eventHandlers).forEach(([event, handler]) => {
+      ironRV.addListener(event, handler);
     });
 
     window.showRV = () => {
       ironRV.show();
     };
   });
-}
+};
